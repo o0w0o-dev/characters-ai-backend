db577f8e85ac38ba9f6ef1357905db4cc0534224
o0w0o-dev <o0w0o.dev@gmail.com>
2024-11-28 16:56:50 +0800

    feat: apply router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import { router as characterRouter } from "./routes/characterRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -13,139 +14,7 @@ app.use(morgan(":method :url :status - :response-time ms"));
 const ENV = process.env;
 const port = ENV.PORT || 8000;
 
-// TODO: remove dev data
-const characters = [
-  {
-    id: "1",
-    name: "characterName1",
-    model: "characterModel1",
-    instructions: "characterInstructions1",
-    avatar: "characterAvatar1",
-    createAt: Date.now(),
-  },
-  {
-    id: "2",
-    name: "characterName2",
-    model: "characterModel2",
-    instructions: "characterInstructions2",
-    avatar: "characterAvatar2",
-    createAt: Date.now(),
-  },
-  {
-    id: "3",
-    name: "characterName3",
-    model: "characterModel3",
-    instructions: "characterInstructions3",
-    avatar: "characterAvatar3",
-    createAt: Date.now(),
-  },
-  {
-    id: "4",
-    name: "characterName4",
-    model: "characterModel4",
-    instructions: "characterInstructions4",
-    avatar: "characterAvatar4",
-    createAt: Date.now(),
-  },
-];
-
-function getAllCharacters(req, res) {
-  try {
-    res.status(200).json({
-      status: "success",
-      results: characters.length,
-      data: { characters },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-}
-
-function getCharacter(req, res) {
-  try {
-    const id = req.params.id;
-    const character = characters.find((character) => character.id === id);
-
-    if (!character) {
-      res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    } else {
-      res.status(200).json({
-        status: "success",
-        data: { character },
-      });
-    }
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-}
-
-function createCharacter(req, res) {
-  try {
-    res.status(201).json({
-      status: "success",
-      data: req.body,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-}
-
-function updateCharacter(req, res) {
-  try {
-    res.status(201).json({
-      status: "success",
-      data: req.body,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-}
-
-function deleteCharacter(req, res) {
-  try {
-    const id = req.params.id;
-    const character = characters.find((character) => character.id === id);
-
-    if (!character) {
-      res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-      });
-    } else {
-      res.status(204).json({
-        status: "success",
-        data: null,
-      });
-    }
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-}
-
-app.route("/api/v1/characters").get(getAllCharacters).post(createCharacter);
-app
-  .route("/api/v1/characters/:id")
-  .get(getCharacter)
-  .put(updateCharacter)
-  .delete(deleteCharacter);
+app.use("/api/v1/characters", characterRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
