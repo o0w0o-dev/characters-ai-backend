e7f24eb1645cd26e19a323ba81ba3820c7977037
o0w0o-dev <o0w0o.dev@gmail.com>
2024-11-28 17:07:52 +0800

    merge: feat/characters-api

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 
 import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+import { router as characterRouter } from "./routes/characterRoutes.js";
 
 const app = express();
-
 dotenv.config();
+
+app.use(express.json());
+app.use(morgan(":method :url :status - :response-time ms"));
 
 const ENV = process.env;
 const port = ENV.PORT || 8000;
 
+app.use("/api/v1/characters", characterRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).json({
+    root: true,
+  });
 });
 
 app.listen(port, () => {
---
+++ routes/characterRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getAllCharacters,
+  getCharacter,
+  createCharacter,
+  updateCharacter,
+  deleteCharacter,
+} from "../controllers/characterController.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllCharacters).post(createCharacter);
+router
+  .route("/:id")
+  .get(getCharacter)
+  .put(updateCharacter)
+  .delete(deleteCharacter);
+
+export { router };
