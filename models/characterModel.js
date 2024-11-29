7ded37ae7f8fee9258ea4f6f87d24644dca21b99
o0w0o-dev <o0w0o.dev@gmail.com>
2024-11-29 16:27:02 +0800

    feat: add characterModel

---
+++ models/characterModel.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const characterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must have a name"],
+    unique: true,
+    trim: true,
+    maxlength: [25, "The name must have less or equal then 25 characters"],
+    minlength: [4, "The name must have more or equal then 4 characters"],
+  },
+  model: {
+    type: String,
+    default: "Model_A",
+    enum: {
+      values: ["Model_A", "Model_B", "Model_C"], // TODO: remove dev data
+      message: "Model is either: Model_A, Model_B, Model_C",
+    },
+  },
+  instructions: {
+    type: String,
+    trim: true,
+    default: "I want you act as  a helpful assistant.",
+    maxlength: [
+      250,
+      "The instructions must have less or equal then 250 characters",
+    ],
+    minlength: [
+      4,
+      "The instructions must have more or equal then 4 characters",
+    ],
+  },
+  avatar: {
+    type: String,
+    trim: true,
+    default: "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg", // TODO: remove dev data
+  },
+  //   userId: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'User',
+  //     required: [true, "Must belong to a user"] // TODO: add user id
+  //   },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated_at: {
+    type: Date,
+  },
+  deleted_at: {
+    type: Date,
+  },
+});
+
+const Character = mongoose.model("Character", characterSchema);
+
+export { Character };
