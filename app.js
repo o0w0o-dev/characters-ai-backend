2f945d461a22b50b820276771f2a6da67c7aaa46
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 14:44:55 +0800

    feat: add rateLimit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import express from "express";
+import rateLimit from express-rate-limit
 import morgan from "morgan";
 import { router as characterRouter } from "./routes/characterRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
@@ -9,6 +10,14 @@ import { globalErrorHandler } from "./controllers/errorController.js";
 
 const app = express();
 dotenv.config();
+
+const limiter = rateLimit({
+	max: process.env.RATE_LIMIT * 1,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP."
+})
+
+app.use("*", limiter)
 
 app.use(express.json());
 app.use(morgan(":method :url :status - :response-time ms"));
