619bf833dd3c434bcb8a136dd53c5ce96cb234b5
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 15:01:45 +0800

    fix: modules import

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 import dotenv from "dotenv";
 import express from "express";
-import helmet from "helmet"
-import hpp from "hpp"
-import mongoSanitize from "mongoSanitize"
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoSanitize from "express-mongo-sanitize";
 import morgan from "morgan";
-import rateLimit from express-rate-limit
-import xss from xss-clean
+import rateLimit from "express-rate-limit";
+import xss from "xss-clean";
 import { router as characterRouter } from "./routes/characterRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
 import { globalErrorHandler } from "./controllers/errorController.js";
@@ -15,20 +15,20 @@ import { globalErrorHandler } from "./controllers/errorController.js";
 const app = express();
 dotenv.config();
 
-app.use(helmet())
+app.use(helmet());
 
 const limiter = rateLimit({
-	max: process.env.RATE_LIMIT * 1,
-	windowMs: 60 * 60 * 1000,
-	message: "Too many requests from this IP."
-})
+  max: process.env.RATE_LIMIT * 1,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP.",
+});
 
-app.use("*", limiter)
-app.use(mongoSanitize())
-app.use(xss())
-app.use(hpp())
+app.use("*", limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
-app.use(express.json({ limit: '10kb' }))
+app.use(express.json({ limit: "10kb" }));
 app.use(morgan(":method :url :status - :response-time ms"));
 
 app.use("/api/v1/characters", characterRouter);
