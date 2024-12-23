f227e80e222bdf54d370292b0252b1576aa35e09
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 10:28:14 +0800

    feat: handle uncaughtException and unhandledRejection

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config();
 
 const ENV = process.env;
@@ -21,6 +27,14 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
