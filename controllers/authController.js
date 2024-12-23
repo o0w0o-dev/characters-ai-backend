68b15bc969ccf3f01476943778e85fbae78f43af
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 13:29:06 +0800

    feat: apply jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import jwt from "jsonwebtoken";
 import { AppError } from "../utils/appError.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import { User } from "./../models/userModel.js";
@@ -7,8 +8,13 @@ import { User } from "./../models/userModel.js";
 function responseWithUser(user, statusCode, res) {
   user = { id: user._id, proUser: user.proUser };
 
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(statusCode).json({
     status: "success",
+    token,
     data: { user },
   });
 }
