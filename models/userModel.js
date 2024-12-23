ffbb3c5e020ab19845825d75c23981b6df6a98ce
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 10:40:23 +0800

    feat: add userModel

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  proUser: {
+    type: Boolean,
+    default: false,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same!",
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export { User };
