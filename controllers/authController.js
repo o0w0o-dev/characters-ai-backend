821ca2bf062ac1f2ddd3542140cab19eb558e8ca
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 14:38:43 +0800

    feat: add cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,9 +8,19 @@ import { User } from "./../models/userModel.js";
 function responseWithUser(user, statusCode, res) {
   user = { id: user._id, proUser: user.proUser };
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
