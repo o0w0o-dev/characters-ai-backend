a439b23a968c9f3952c48e3f1dc7f0e6956a9a4e
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-23 13:58:40 +0800

    feat: add sendEmail

---
+++ utils/email.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "o0w0o <no-reply@o0w0o.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
