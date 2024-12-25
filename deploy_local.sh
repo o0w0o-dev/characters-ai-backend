108d63ed44e38a37bf90794c30bf6d1bb500b2a7
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-25 12:35:36 +0800

    feat: env required with deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-CONTAINER_NAME="characters-ai-backend"
+# Check if an argument was provided
+if [ $# -eq 0 ]; then
+    echo "No env provided."
+    echo "Usage: $0 <env>"
+    exit 1
+fi
+
+# The first argument is the env
+ENV=$1
+echo "ENV: $ENV"
+
+FOLDER_NAME="characters-ai-backend"
+CONTAINER_NAME="characters-ai-${ENV}-backend"
 
 # save the current working directory
 LOCAL_FOLDER_PATH=$(pwd)
@@ -9,14 +21,19 @@ LOCAL_FOLDER_PATH=$(pwd)
 BASE_NAME=$(basename "$LOCAL_FOLDER_PATH")
 
 # if the base name is $CONTAINER_NAME
-if [ "$BASE_NAME" == ${CONTAINER_NAME} ]; then
-    echo "The name of current working directory is '${CONTAINER_NAME}'."
+if [ "$BASE_NAME" == ${FOLDER_NAME} ]; then
+    echo "The name of current working directory is '${FOLDER_NAME}'."
 else
-    echo "The name of current working directory is not '${CONTAINER_NAME}'."
+    echo "The name of current working directory is not '${FOLDER_NAME}'."
     exit 1
 fi
 
 export CONTAINER_NAME=$CONTAINER_NAME
+
+# sops -d ./ansible/$ENV/secrets.enc.env > .env
+cp ./ansible/$ENV/.env .env
+cp ./ansible/$ENV/Dockerfile.$ENV Dockerfile
+cp ./ansible/$ENV/docker-compose.$ENV.yml docker-compose.yml
 
 # remove container if running
 docker rm -f ${CONTAINER_NAME} || true
