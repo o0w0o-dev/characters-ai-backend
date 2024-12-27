a7b402bf0d7390e2959dafc6774aa5ad879ba068
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-27 16:37:16 +0800

    feat: update base_url

--- test/tests/test.spec.js
+++ test/tests/test.spec.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const BASE_URL = `http://${process.env.HOST_DNS}:${process.env.PORT}`;
 
 // Wait for server started
 test.beforeEach(async () => {
@@ -15,7 +15,7 @@ test.beforeEach(async () => {
 const headers = { accept: "application/json" };
 
 test("example test", async () => {
-  const response = await fetch(`http://localhost:${PORT}`, {
+  const response = await fetch(`${BASE_URL}`, {
     headers,
   });
   expect(response.status).toBe(200);
