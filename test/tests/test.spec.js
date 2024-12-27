ed2b2f1e12a10ea1b3431c178bb0d6ac6e4bd155
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-27 16:57:35 +0800

    feat: add signup test case

--- test/tests/test.spec.js
+++ test/tests/test.spec.js
@@ -12,12 +12,28 @@ test.beforeEach(async () => {
   await new Promise((resolve) => setTimeout(resolve, 10000));
 });
 
-const headers = { accept: "application/json" };
-
 test("example test", async () => {
-  const response = await fetch(`${BASE_URL}`, {
+  const url = BASE_URL;
+  const headers = { accept: "application/json" };
+
+  const response = await fetch(url, {
     headers,
   });
+
+  expect(response.status).toBe(200);
+
+  const data = await response.json();
+  expect(data).toEqual({ root: true });
+});
+
+// User /api/v1/users/signup
+test("signup", async () => {
+  const url = `${BASE_URL}/api/v1/users/signup`;
+  const headers = { accept: "application/json" };
+  const body = {};
+  const method = "POST";
+
+  const response = await fetch(url, { method, headers, body });
   expect(response.status).toBe(200);
 
   const data = await response.json();
