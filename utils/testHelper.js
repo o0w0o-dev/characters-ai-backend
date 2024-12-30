d28e03de072b3fe17fa4ea86be7aee0a70cb54ed
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-30 10:15:54 +0800

    feat: add test case helper

---
+++ utils/testHelper.js
@@ -0,0 +1,45 @@
+"use strict";
+
+export async function getTestResponse(
+  url,
+  method,
+  body = {},
+  headers = { "Content-Type": "application/json" }
+) {
+  const response = await fetch(url, {
+    method,
+    headers,
+    body: JSON.stringify(body),
+  });
+
+  return response;
+}
+
+export async function verifyResult(
+  expect,
+  response,
+  expectedStatusCode,
+  expectedStatus,
+  expectedMessage = undefined
+) {
+  expect(response.status).toBe(expectedStatusCode);
+
+  const data = await response.json();
+  expect(data.status).toEqual(expectedStatus);
+  if (expectedMessage) expect(data.message).toEqual(expectedMessage);
+}
+
+export async function exampleTest(test, expect, url) {
+  test("example test", async () => {
+    const headers = { "Content-Type": "application/json" };
+
+    const response = await fetch(url, {
+      headers,
+    });
+
+    expect(response.status).toBe(200);
+
+    const data = await response.json();
+    expect(data).toEqual({ root: true });
+  });
+}
