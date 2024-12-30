23129a2c7239df5f4d7a13330c59947438f6f0fc
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-30 12:31:29 +0800

    feat: update getUser test case

--- test/tests/getUser.spec.js
+++ test/tests/getUser.spec.js
@@ -50,12 +50,11 @@ test.beforeAll(async () => {
   await init();
 });
 
-test.describe.serial("getUser test cases", () => {
+test.describe("getUser test cases", () => {
   exampleTest(test, expect, BASE_URL);
 
   test("getUser", async () => {
     const data = await login();
-    console.log(data);
     const response = await getTestResponse(
       `http://${process.env.HOST_DNS}:${process.env.PORT}/api/v1/users/${data.data?.user?.id}`,
       "GET",
@@ -66,5 +65,73 @@ test.describe.serial("getUser test cases", () => {
       }
     );
     await verifyResult(expect, response, 200, "success");
+  });
+
+  test("getUser without id", async () => {
+    const data = await login();
+    const response = await getTestResponse(
+      `http://${process.env.HOST_DNS}:${process.env.PORT}/api/v1/users/`,
+      "GET",
+      undefined,
+      {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${data.token}`,
+      }
+    );
+    await verifyResult(
+      expect,
+      response,
+      404,
+      "fail",
+      "Cannot find /api/v1/users/ on this server."
+    );
+  });
+
+  test("getUser with wrong id", async () => {
+    const data = await login();
+    const wrongId = data.data?.user?.id.split("").sort().join("");
+    const response = await getTestResponse(
+      `http://${process.env.HOST_DNS}:${process.env.PORT}/api/v1/users/${wrongId}`,
+      "GET",
+      undefined,
+      {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${data.token}`,
+      }
+    );
+    await verifyResult(expect, response, 404, "fail", "Invalid ID");
+  });
+
+  test("getUser without auth", async () => {
+    const data = await login();
+    const response = await getTestResponse(
+      `http://${process.env.HOST_DNS}:${process.env.PORT}/api/v1/users/${data.data?.user?.id}`,
+      "GET",
+      undefined,
+      {
+        "Content-Type": "application/json",
+      }
+    );
+    await verifyResult(
+      expect,
+      response,
+      401,
+      "fail",
+      "You are not logged in! Please log in to get access"
+    );
+  });
+
+  test("getUser with wrong jwt token", async () => {
+    const data = await login();
+    const response = await getTestResponse(
+      `http://${process.env.HOST_DNS}:${process.env.PORT}/api/v1/users/${data.data?.user?.id}`,
+      "GET",
+      undefined,
+      {
+        "Content-Type": "application/json",
+        Authorization: `Bearer wrong token`,
+      }
+    );
+    await verifyResult(expect, response, 500, "error", "jwt malformed");
   });
 });
