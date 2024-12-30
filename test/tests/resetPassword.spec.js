4707ab41e13f9a9ac2dec37cf0d648e3b6e4bc77
o0w0o-dev <o0w0o.dev@gmail.com>
2024-12-30 10:15:23 +0800

    feat: refactor test cases

--- test/tests/resetPassword.spec.js
+++ test/tests/resetPassword.spec.js
@@ -1,6 +1,11 @@
 "use strict";
 
 import { test, expect } from "@playwright/test";
+import {
+  getTestResponse,
+  verifyResult,
+  exampleTest,
+} from "./../../utils/testHelper.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,33 +15,20 @@ const password = "12345678Abc";
 const passwordConfirm = "12345678Abc";
 
 test.describe.serial("resetPassword test cases", () => {
-  test("example test", async () => {
-    const url = BASE_URL;
-    const headers = { "Content-Type": "application/json" };
-
-    const response = await fetch(url, {
-      headers,
-    });
-
-    expect(response.status).toBe(200);
-
-    const data = await response.json();
-    expect(data).toEqual({ root: true });
-  });
+  exampleTest(test, expect, BASE_URL);
 
   test("resetPassword without token", async () => {
     const token = "";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ password, passwordConfirm });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(404);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual(
+    const response = await getTestResponse(url, "PATCH", {
+      password,
+      passwordConfirm,
+    });
+    await verifyResult(
+      expect,
+      response,
+      404,
+      "fail",
       "Cannot find /api/v1/users/resetPassword/ on this server."
     );
   });
@@ -44,76 +36,72 @@ test.describe.serial("resetPassword test cases", () => {
   test("resetPassword with invalid token", async () => {
     const token = "fakeToken";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ password, passwordConfirm });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(400);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual("Token is invalid or has expired");
+    const response = await getTestResponse(url, "PATCH", {
+      password,
+      passwordConfirm,
+    });
+    await verifyResult(
+      expect,
+      response,
+      400,
+      "fail",
+      "Token is invalid or has expired"
+    );
   });
 
   test("resetPassword with empty body", async () => {
     const token = "tempToken";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({});
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(400);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual("Please provide valid password");
+    const response = await getTestResponse(url, "PATCH", {});
+    await verifyResult(
+      expect,
+      response,
+      400,
+      "fail",
+      "Please provide valid password"
+    );
   });
 
   test("resetPassword without password", async () => {
     const token = "tempToken";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ passwordConfirm });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(400);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual("Please provide valid password");
+    const response = await getTestResponse(url, "PATCH", { passwordConfirm });
+    await verifyResult(
+      expect,
+      response,
+      400,
+      "fail",
+      "Please provide valid password"
+    );
   });
 
   test("resetPassword without passwordConfirm", async () => {
     const token = "tempToken";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ password });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(400);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual("Please provide valid password");
+    const response = await getTestResponse(url, "PATCH", { password });
+    await verifyResult(
+      expect,
+      response,
+      400,
+      "fail",
+      "Please provide valid password"
+    );
   });
 
   test("resetPassword with wrong password", async () => {
     const token = "tempToken";
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ password: password + "1", passwordConfirm });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(400);
-
-    const data = await response.json();
-    expect(data.status).toEqual("fail");
-    expect(data.message).toEqual("Please provide valid password");
+    const response = await getTestResponse(url, "PATCH", {
+      password: password + "1",
+      passwordConfirm,
+    });
+    await verifyResult(
+      expect,
+      response,
+      400,
+      "fail",
+      "Please provide valid password"
+    );
   });
 
   // TODO: manual test cases
@@ -122,14 +110,10 @@ test.describe.serial("resetPassword test cases", () => {
     test.skip(!token); // skip test if no token provided
 
     const url = `${BASE_URL}/api/v1/users/resetPassword/${token}`;
-    const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ password, passwordConfirm });
-    const method = "PATCH";
-
-    const response = await fetch(url, { method, headers, body });
-    expect(response.status).toBe(200);
-
-    const data = await response.json();
-    expect(data.status).toEqual("success");
+    const response = await getTestResponse(url, "PATCH", {
+      password,
+      passwordConfirm,
+    });
+    await verifyResult(expect, response, 200, "success");
   });
 });
